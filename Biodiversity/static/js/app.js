98291a3988bc7b28cdfd32cf696643f1bb3c4334
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-09-06 13:35:51 -0400

    adding fill_panel full funcionality

--- Biodiversity/static/js/app.js
+++ Biodiversity/static/js/app.js
@@ -6,7 +6,15 @@ function fill_panel (sub_id) {
     current_subj = biodiv.metadata.find(val=>val.id==sub_id);
     d3.select("#subj_id").text("id: " + current_subj.id)
     d3.select("#subj_eth").text("ethnicity: " + current_subj.ethnicity)
-  
+    d3.select("#subj_gen").text("gender: " + current_subj.gender)
+    d3.select("#subj_age").text("age: " + current_subj.age)
+    d3.select("#subj_loc").text("location: " + current_subj.location)
+    d3.select("#subj_bbt").text("bbtype: " + current_subj.bbtype)
+    d3.select("#subj_wfr").text("wfreq: " + current_subj.wfreq)
+   
+   
+
+    
     console.log(current_subj);
 }
 
